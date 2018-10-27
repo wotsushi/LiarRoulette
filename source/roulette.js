36bdeb1b7b7ba5fd3549c187faff82f52169d3d3
Kota-iwadate <beeguru122@gmail.com>
2018-10-27 19:22:38 +0900

    ルーレットの実行結果にGUIから渡したparameter(確率分布)を反映

--- source/roulette.js
+++ source/roulette.js
@@ -1,6 +1,6 @@
-$(function(){
+function roulette(param1, param2, param3, param4){
 var speed = 10;   //ルーレットの回転速度
-  var divide = 5;   //ルーレットの分割数
+  var divide = 4;   //ルーレットの分割数
   var timeout = 2000;   //○秒後に停止
  
   //停止位置の設定。1～360までの乱数を取得して挿入する
@@ -8,14 +8,23 @@ var speed = 10;   //ルーレットの回転速度
  
   //ルーレットの角度の変数。停止位置の値を初期値に設定する
   var angle = stopAngle;
+
+  var stopNumber = 1;
  
-  //ルーレットの分割数から1エリア分の角度を求める。今回は5分割なので72が入ってます
-  var section = 360/divide;
- 
+  //
+  var sections = 360/divide;
+  //レート
+  var rates = [param1, param2, param3, param4]
+
   //停止位置がどのエリアにあるか調べ、該当する番号をstopNumberに格納
-  for(i=1; i<=divide; i++){
-     if(section*(i-1)+1 <= stopAngle && stopAngle <= section*i) {
-        stopNumber = i;
+  var accumulated_rate = 0;
+  accumulated_rate += rates[0];
+  for(i=1; i<divide; i++){
+     if( accumulated_rate < stopAngle) {
+        stopNumber++;
+        accumulated_rate += rates[i];
+     }else{
+        break;
      }
     };
 
@@ -69,9 +78,36 @@ setTimeout(function(){
     $("#hari img").attr('src', $("#hari img").attr('src').replace('gif', 'png'));
   
   }, timeout);
+  //ルーレット停止後に実行される処理
+  var result = function(){
+    switch(stopNumber){
+
+       //1の時の処理
+       case 1:
+         $("#result span").text("①でした！")
+         break;
+
+       //2の時の処理
+       case 2:
+         $("#result span").text("②でした！")
+         break;
+
+       //3の時の処理
+       case 3:
+         $("#result span").text("③でした！")
+         break;
+
+       //4の時の処理
+       case 4:
+         $("#result span").text("④でした！")
+         break;
+
+       //5の時の処理
+       case 5:
+         $("#result span").text("⑤でした！")
+         break;
+    };
+ };
  
  
-
-
- rotate();
-});
+};
