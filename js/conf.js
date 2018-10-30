cdc1d6db55c927cc49638b4747a3de80b15e6e9d
wotsushi <wotsushi@gmail.com>
2018-10-30 22:43:18 +0900

    Fix loading conf procedure

--- js/conf.js
+++ js/conf.js
@@ -1,13 +1,16 @@
 function updateRoulette() {
   var succNum = $('#confTable').prop('rows').length
+  localStorage.setItem('areaNames', JSON.stringify(
+    _.range(1, succNum).map(i => $(`#areaName${i}`).text())
+  ))
   localStorage.setItem('areas', JSON.stringify(
-    _.range(succNum).map(i => Number($(`#area${i}`).val()))
+    _.range(1, succNum).map(i => Number($(`#area${i}`).val()))
   ))
   localStorage.setItem('probA', JSON.stringify(
-    _.range(succNum).map(i => Number($(`#probA${i}`).val()))
+    _.range(1, succNum).map(i => Number($(`#probA${i}`).val()))
   ))
   localStorage.setItem('probB', JSON.stringify(
-    _.range(succNum).map(i => Number($(`#probB${i}`).val()))
+    _.range(1, succNum).map(i => Number($(`#probB${i}`).val()))
   ))
   alert('Roulette settings are updated!')
 }
@@ -15,8 +18,8 @@ function updateRoulette() {
 function addParamRow() {
   var areaName = $('#areaN').val()
   var succNum = $('#confTable').prop('rows').length
-  $('#confTable tbody > tr:last').after(`<tr>\
-                                         <th scope="row">${areaName}</th>\
+  $('#confTable tbody').append(`<tr>\
+                                         <th scope="row" id="areaName${succNum}">${areaName}</th>\
                                          <td><input type="number" class="form-control input-normal" placeholder="0" id="area${succNum}"></td>\
                                          <td><input type="number" class="form-control input-normal" placeholder="0" id="probA${succNum}"></td>\
                                          <td><input type="number" class="form-control input-normal" placeholder="0" id="probB${succNum}"></td>\
@@ -24,7 +27,13 @@ function addParamRow() {
 }
 
 $(() => {
-  JSON.parse(localStorage.getItem('areas')).forEach((area, i) => $(`#area${i}`).val(area))
-  JSON.parse(localStorage.getItem('probA')).forEach((probA, i) => $(`#probA${i}`).val(probA))
-  JSON.parse(localStorage.getItem('probB')).forEach((probB, i) => $(`#probB${i}`).val(probB))
-  });
+  let areaNames = localStorage.getItem('areaNames')
+  if (areaNames) {
+    let n = JSON.parse(areaNames).length
+    _.range(n).forEach(i => addParamRow())
+    JSON.parse(localStorage.getItem('areaNames')).forEach((areaName, i) => $(`#areaName${i + 1}`).text(areaName))
+    JSON.parse(localStorage.getItem('areas')).forEach((area, i) => $(`#area${i + 1}`).val(area))
+    JSON.parse(localStorage.getItem('probA')).forEach((probA, i) => $(`#probA${i + 1}`).val(probA))
+    JSON.parse(localStorage.getItem('probB')).forEach((probB, i) => $(`#probB${i + 1}`).val(probB))
+  }
+});
