16707a8771acdb7d225b15cd71e38e50e894102a
Kota-iwadate <beeguru122@gmail.com>
2018-10-30 21:37:37 +0900

    change conf table length to variable

--- js/conf.js
+++ js/conf.js
@@ -1,14 +1,25 @@
 function updateRoulette() {
+  var succNum = $('#confTable').prop('rows').length
   localStorage.setItem('areas', JSON.stringify(
-    _.range(4).map(i => Number($(`#area${i}`).val()))
+    _.range(succNum).map(i => Number($(`#area${i}`).val()))
   ))
   localStorage.setItem('probA', JSON.stringify(
-    _.range(4).map(i => Number($(`#probA${i}`).val()))
+    _.range(succNum).map(i => Number($(`#probA${i}`).val()))
   ))
   localStorage.setItem('probB', JSON.stringify(
-    _.range(4).map(i => Number($(`#probB${i}`).val()))
+    _.range(succNum).map(i => Number($(`#probB${i}`).val()))
   ))
   alert('Roulette settings are updated!')
+}
+
+function addParamRow() {
+  var succNum = $('#confTable').prop('rows').length
+  $('#confTable tbody > tr:last').after(`<tr>\
+                                         <th scope="row">Param${succNum}</th>\
+                                         <td><input type="number" class="form-control input-normal" placeholder="0" id="area${succNum}"></td>\
+                                         <td><input type="number" class="form-control input-normal" placeholder="0" id="probA${succNum}"></td>\
+                                         <td><input type="number" class="form-control input-normal" placeholder="0" id="probB${succNum}"></td>\
+                                         </tr>`)
 }
 
 $(() => {
