855399781d63c51e419cf92945a81991a0603636
wotsushi <wotsushi@gmail.com>
2018-11-02 23:16:47 +0900

    Improve swipe UI

--- js/roulette.js
+++ js/roulette.js
@@ -1,26 +1,26 @@
-var areas = JSON.parse(localStorage.getItem('areas'));
-var probs = JSON.parse(localStorage.getItem('probA'));
 const speed = 10;   //ルーレットの回転速度
 const timeout = 2000;   //○秒後に停止
 
 // ルーレットの回転処理
-function playRoulette() {
-  let x = _.random(1, _.sum(probs))
-  let accProbs = _.concat(0, _.range(1, probs.length + 1).map(i => _.sum(_.take(probs, i))))
+function playRoulette(roulette) {
+  let probs = JSON.parse(localStorage.getItem(`prob${roulette}`));
+  let x = _.random(1, _.sum(probs));
+  let accProbs = _.concat(0, _.range(1, probs.length + 1).map(i => _.sum(_.take(probs, i))));
   let stopNumber = _.find(
     _.range(1, probs.length + 1),
     i => accProbs[i] >= x
-  )
-  let normalizedArea = JSON.parse(localStorage.getItem('areas')).map(area => 360 / _.sum(areas) * area)
+  );
+  let areas = JSON.parse(localStorage.getItem('areas'));
+  let normalizedArea = areas.map(area => 360 / _.sum(areas) * area);
 
   //停止位置の設定。1～360までの乱数を取得して挿入する
-  var stopAngle = 360 - _.sum(_.take(_.concat(0, normalizedArea), stopNumber)) - _.random(normalizedArea[stopNumber - 1])
+  var stopAngle = 360 - _.sum(_.take(_.concat(0, normalizedArea), stopNumber)) - _.random(normalizedArea[stopNumber - 1]);
 
   //ルーレットの角度の変数。停止位置の値を初期値に設定する
   var angle = stopAngle;
 
   var rotation = setInterval(function(){
-    $("#chart-area").rotate(angle);
+    $(`#roulette${roulette}`).rotate(angle);
     angle += speed;
   }, 5);
 
@@ -33,19 +33,18 @@ function playRoulette() {
     angle = angle%360-360;
 
     //停止位置までのアニメーション。完了するとresult()が実行される
-    $("#chart-area").rotate({
+    $(`#roulette${roulette}`).rotate({
       angle: angle,
       animateTo: stopAngle,
-      callback: () => $("#result span").text(`${JSON.parse(localStorage.getItem('areaNames'))[stopNumber - 1]}でした！`)
+      callback: () => $(`#result${roulette} span`).text(
+        `${JSON.parse(localStorage.getItem('areaNames'))[stopNumber - 1]}でした！`)
     });
-    $("#hari img").attr('src', $("#hari img").attr('src').replace('gif', 'png'));
+    $(`#hari${roulette} img`).attr('src', $(`#hari${roulette} img`).attr('src').replace('gif', 'png'));
   }, timeout);
 };
 
-$(() => {
-  var ctx = document.getElementById('chart-area').getContext('2d');
-  ctx.canvas.width = 500;
-  window.myPie = new Chart(ctx, {
+function createRoulette(roulette) {
+  return new Chart(document.getElementById(`roulette${roulette}`).getContext('2d'), {
     type: 'pie',
     data: {
       datasets: [{
@@ -55,8 +54,7 @@ $(() => {
           window.chartColors.yellow,
           window.chartColors.green,
           window.chartColors.blue,
-        ],
-        label: 'data1'
+        ]
       }],
       labels: ['a', 'b', 'c', 'd']
     },
@@ -65,16 +63,21 @@ $(() => {
       legend: { display: false}
     }
   });
-  $("#main").swipe( {
-    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-      if (direction === 'right') {
-        alert('switch B');
-        probs = JSON.parse(localStorage.getItem('probB'));
-      }
-      if (direction === 'left') {
-        alert('switch A');
-        probs = JSON.parse(localStorage.getItem('probA'));
+}
+
+$(() => {
+  let rouletteA = createRoulette('A');
+  let rouletteB = createRoulette('B');
+  let swiper = new Swiper('.swiper-container', {
+    initialSlide: 1,
+    on: {
+      slideChange: () => {
+        rouletteA.data.datasets[0].data = JSON.parse(localStorage.getItem('areas'))
+        rouletteA.update()
+        rouletteB.data.datasets[0].data = JSON.parse(localStorage.getItem('areas'))
+        rouletteB.update()
       }
     }
   });
+
 });
